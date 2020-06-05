26c9507ba0ede746b6787a171d3b251738407b27
mehdi <m.mhmvd.id@gmail.com>
2020-06-05 12:45:41 +0300

    Initial commit

---
+++ src/main/java/com/ludus/tutapp/TutappApplication.java
@@ -0,0 +1,13 @@
+package com.ludus.tutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutappApplication.class, args);
+    }
+
+}
